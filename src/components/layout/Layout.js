bb9452fb5b8a23f0da4b9e96717421f219bbf294
Alireza Fereydoni <35655080+AlirezaFereydoni@users.noreply.github.com>
2020-03-13 23:09:55 +0330

    Create Ingredient Burger Component And Write Some Function in that

--- src/components/layout/Layout.js
+++ src/components/layout/Layout.js
@@ -1,10 +1,10 @@
 import React, { Fragment } from "react";
-
+import styles from "./Layout.module.css";
 const Layout = props => {
   return (
     <Fragment>
       <div>toolbar , side drawer back drop</div>
-      <main>{props.children}</main>
+      <main className={styles.basic}>{props.children}</main>
     </Fragment>
   );
 };
