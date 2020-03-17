45c6ab183ebb37e92fafba32f12be3080dff9264
Alireza Fereydoni <35655080+AlirezaFereydoni@users.noreply.github.com>
2020-03-17 14:12:58 +0330

    Improve Performance in Modal

--- src/components/UI/Modal/Modal.js
+++ src/components/UI/Modal/Modal.js
@@ -1,22 +1,28 @@
-import React, { Fragment } from "react";
+import React, { Fragment, Component } from "react";
 import styles from "./Modal.module.css";
 import Backdrop from "../Backdrop/Backdrop";
 
-const Modal = props => {
-  return (
-    <Fragment>
-      <Backdrop clicked={props.modalClosed} show={props.show} />
-      <div
-        className={styles.Modal}
-        style={{
-          transform: props.show ? "translateY(0)" : "translateY(-100vh)",
-          opacity: props.show ? "1" : "0"
-        }}
-      >
-        {props.children}
-      </div>
-    </Fragment>
-  );
-};
+class Modal extends Component {
+  componentShouldUpdate(nextProps, nestState) {
+    return nextProps.show !== this.props.show;
+  }
+
+  render() {
+    return (
+      <Fragment>
+        <Backdrop clicked={this.props.modalClosed} show={this.props.show} />
+        <div
+          className={styles.Modal}
+          style={{
+            transform: this.props.show ? "translateY(0)" : "translateY(-100vh)",
+            opacity: this.props.show ? "1" : "0"
+          }}
+        >
+          {this.props.children}
+        </div>
+      </Fragment>
+    );
+  }
+}
 
 export default Modal;
