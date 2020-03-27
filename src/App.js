21aa394e45e265ff9a1dba74a8d6f9c117b5c5ab
Alireza Fereydoni <35655080+AlirezaFereydoni@users.noreply.github.com>
2020-03-27 15:08:55 +0430

    Install React-router

--- src/App.js
+++ src/App.js
@@ -2,13 +2,16 @@ import React, { Component } from "react";
 import Layout from "./hoc/layout/Layout";
 import BurgerBuilder from "./containers/BurgerBuilder/BurgerBuilder";
 import Checkout from "./containers/Checkout/Checkout";
+import { Route, Switch } from "react-router-dom";
 class App extends Component {
   render() {
     return (
       <div>
         <Layout>
-          <BurgerBuilder />
-          <Checkout />
+          <Switch>
+            <Route path="/checkout" component={Checkout} />
+            <Route path="/" exact component={BurgerBuilder} />
+          </Switch>
         </Layout>
       </div>
     );
