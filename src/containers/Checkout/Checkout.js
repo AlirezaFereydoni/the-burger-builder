bdaa534d3841d9953131db807f4ce1ba7702d635
Alireza Fereydoni <35655080+AlirezaFereydoni@users.noreply.github.com>
2020-04-10 16:46:43 +0430

    Imporving Ux

--- src/containers/Checkout/Checkout.js
+++ src/containers/Checkout/Checkout.js
@@ -1,7 +1,7 @@
 import React, { Component } from "react";
 import CheckoutSummary from "./../../components/Order/CheckoutSummary/CheckoutSummary";
 import ContactData from "./ContactData/ContactData";
-import { Route } from "react-router-dom";
+import { Route, Redirect } from "react-router-dom";
 import { connect } from "react-redux";
 
 class Checkout extends Component {
@@ -18,19 +18,23 @@ class Checkout extends Component {
     this.props.history.replace("/checkout/contact-data");
   };
   render() {
-    return (
-      <div>
-        <CheckoutSummary
-          ingredient={this.props.ings}
-          cancelCheckout={this.checkoutCancelHandler}
-          continueCheckout={this.checkoutContinueHandler}
-        />
-        <Route
-          path={this.props.match.path + "/contact-data"}
-          component={ContactData}
-        />
-      </div>
-    );
+    let summary = <Redirect to="/" />;
+    if (this.props.ings) {
+      summary = (
+        <div>
+          <CheckoutSummary
+            ingredient={this.props.ings}
+            cancelCheckout={this.checkoutCancelHandler}
+            continueCheckout={this.checkoutContinueHandler}
+          />
+          <Route
+            path={this.props.match.path + "/contact-data"}
+            component={ContactData}
+          />
+        </div>
+      );
+    }
+    return summary;
   }
 }
 
