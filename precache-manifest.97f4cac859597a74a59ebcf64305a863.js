61582f89c24e9c7c8931f45e99454b4b9de536ef
AlirezaFereydoni <AlirezaFereydoni@outlook.com>
2021-05-04 22:54:15 +0430

    Updates

---
+++ precache-manifest.97f4cac859597a74a59ebcf64305a863.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9c982bc1483093442426b8ac644fab3e",
+    "url": "/The-Burger-Builder/index.html"
+  },
+  {
+    "revision": "cded5a868bf980a3a230",
+    "url": "/The-Burger-Builder/static/css/main.bd1b5fb1.chunk.css"
+  },
+  {
+    "revision": "b203156c8a8f532a99e1",
+    "url": "/The-Burger-Builder/static/js/2.b59367a1.chunk.js"
+  },
+  {
+    "revision": "4f2dedb9f87ffe36ce515bb1189ae74f",
+    "url": "/The-Burger-Builder/static/js/2.b59367a1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cded5a868bf980a3a230",
+    "url": "/The-Burger-Builder/static/js/main.68641044.chunk.js"
+  },
+  {
+    "revision": "95ad8f159d3a5b11003b",
+    "url": "/The-Burger-Builder/static/js/runtime-main.984d099a.js"
+  },
+  {
+    "revision": "b8503d262bffbfb7c67fd6762963e7d1",
+    "url": "/The-Burger-Builder/static/media/28.1 burger-logo.png.b8503d26.png"
+  }
+]);
